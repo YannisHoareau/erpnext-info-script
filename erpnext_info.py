017914737711d3c897ffc5bad6239e5a49757d1d
yannis <yannishoareau26@gmail.com>
2025-05-21 17:36:25 +0200

    feat: Apps working with bench

--- erpnext_info.py
+++ erpnext_info.py
@@ -1,49 +1,62 @@
+import os
 import sys
 import json
 
 ALLOWED_ARGS = ["-h", "--help", "-j", "--json"]
 
-if __name__ == 'erpnext_info':
-    argv = sys.argv[1:]
-    args = argv[1:-1]
+if __name__ == '__main__':
+	argv = sys.argv[1:]
+	args = argv[:-1]
 
-    unallowed_arg = False
+	unallowed_arg = False
 
-    for arg in args:
-        if arg not in ALLOWED_ARGS:
-            unallowed_arg = True
+	for arg in args:
+		if arg not in ALLOWED_ARGS:
+			unallowed_arg = True
 
-    if "-h" in args or "--help" in args or unallowed_arg:
-        print('''
+	if "-h" in args or "--help" in args or unallowed_arg:
+		print('''
         Usage: python3 erpnext-info.py [OPTIONS] ERPNEXT_PATH
             -j, --json: Outputs json in cli.
             -h, --help: This usage screen.
         '''.strip())
-        # exit()
+		# exit()
 
-    res_array = dict()
-    app_path = argv[-1]
+	from bench.bench import Bench
+	from bench.utils.app import get_current_version
 
-    import frappe
-    from frappe import cint
+	old_cwd = os.getcwd()
+	bench_path = argv[-1]
+	os.chdir(bench_path)
+	res_array = dict()
 
-    res_sites = dict()
+	working_bench = Bench(bench_path)
 
-    for site in sorted(frappe.utils.get_sites()):
-        res_sites[site] = dict()
-        frappe.init(site)
-        frappe.connect()
-        res_sites[site]["schduler"] = True if cint(frappe.db.get_single_value("System Settings", "enable_scheduler")) == 1 else False
-        res_sites[site]["maintenance"] = True if frappe.local.conf.maintenance_mode == 1 else False
+	site_list = working_bench.sites
+	app_list = working_bench.get_installed_apps()
 
-    res_array["sites"] = res_sites
+	res_sites = dict()
 
-    res_apps = dict()
+	# Next commented part is old implem deprecated now
+	# for site in sorted(frappe.utils.get_sites()):
+	#     res_sites[site] = dict()
+	#     frappe.init(site)
+	#     frappe.connect()
+	#     res_sites[site]["scheduler"] = True if cint(frappe.db.get_single_value("System Settings", "enable_scheduler")) == 1 else False
+	#     res_sites[site]["maintenance"] = True if frappe.local.conf.maintenance_mode == 1 else False
+	#     res_sites[site]["url"] = frappe.utils.get_url()
 
-    for app in sorted(frappe.get_installed_apps()):
-        res_apps[app] = dict()
-        res_apps[app]["version"] = frappe.get_module(app).__version__
+	# res_array["sites"] = res_sites
 
-    res_array["apps"] = res_apps
+	res_apps = dict()
 
-    print(json.dumps(res_array, indent=4))
+	for app_name in app_list:
+		res_apps[app_name] = dict()
+		app_version = get_current_version(app_name, bench_path=working_bench.name)
+		res_apps[app_name]["version"] = app_version
+
+	res_array["apps"] = res_apps
+
+	print(json.dumps(res_array, indent=4))
+	os.chdir(old_cwd)
+	exit()
