05dfe106d7e87424f7d6930b65a0ffb7943dbd48
yannis <yannishoareau26@gmail.com>
2025-05-27 15:46:33 +0200

    feat: factorize for more uses

--- erpnext_info.py
+++ erpnext_info.py
@@ -1,6 +1,27 @@
 import sys
 
 ALLOWED_ARGS = ["-h", "--help", "-j", "--json"]
+res_array = dict()
+
+def create_info_dict(site_list, app_list):
+	res_sites = dict()
+	res_apps = dict()
+
+	for site in sorted(site_list):
+		res_sites[site] = dict()
+		frappe.init(site)
+		frappe.connect()
+		res_sites[site]["scheduler"] = True if cint(frappe.db.get_single_value("System Settings", "enable_scheduler")) == 1 else False
+		res_sites[site]["maintenance"] = True if frappe.local.conf.maintenance_mode == 1 else False
+		res_sites[site]["url"] = get_url()
+
+	for app in app_list:
+		res_apps[app] = {
+			"app_version": frappe.get_module(app).__version__ or "UNVERSIONED"
+		}
+
+	res_array["sites"] = res_sites
+	res_array["apps"] = res_apps
 
 if __name__ == '__main__':
 	argv = sys.argv[1:]
@@ -21,46 +42,27 @@ if __name__ == '__main__':
             -j, --json: Outputs json in cli.
             -h, --help: This usage screen.
         '''.strip())
-		exit()
+		exit(1)
 
 	try:
 		import os
 		import frappe
-		from frappe import cint
-		from frappe.utils import get_sites, get_installed_apps_info, get_url
+		from frappe import cint, get_all_apps
+		from frappe.utils import get_sites, get_url
 	except ImportError:
-		print("Error while importing frappe. Please check your installation.")
-		exit()
+		exit(1)
 
-	res_array = dict()
 	bench_path = argv[-1]
 	old_cwd = os.getcwd()
-
+	path = os.path.join(bench_path, "sites")
 	os.chdir(os.path.join(bench_path, "sites"))
 
-	res_sites = dict()
-
-	for site in sorted(get_sites()):
-		res_sites[site] = dict()
-		frappe.init(site)
-		frappe.connect()
-		res_sites[site]["scheduler"] = True if cint(frappe.db.get_single_value("System Settings", "enable_scheduler")) == 1 else False
-		res_sites[site]["maintenance"] = True if frappe.local.conf.maintenance_mode == 1 else False
-		res_sites[site]["url"] = get_url()
-
-	res_array["sites"] = res_sites
-
-	res_apps = dict()
-
-	for app in get_installed_apps_info():
-		res_apps[app.get("app_name")] = {
-			"app_version": app.get("version") or "UNVERSIONED"
-		}
-
-	res_array["apps"] = res_apps
+	create_info_dict(get_sites(), get_all_apps(with_internal_apps=False, sites_path=path))
 
 	if print_json:
 		import json
 		print(json.dumps(res_array, indent=4))
+		exit(0)
+
 	os.chdir(old_cwd)
 	exit()
